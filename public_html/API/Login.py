38f1480b35333baf8bb2b7285bcb81d7e35998ca
JayPC <jb.lambert23@gmail.com>
2018-01-28 16:49:28 -0500

    Created mysql setup script and added API dir.

---
+++ public_html/API/Login.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding: utf-8
+
+json_string = '{"user": "db-sys", "password": ""}'
+
+# Parse json
+import json
+parsed_json = json.loads(json_string)
+
+# Connect to the database.
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    db='jayTest',
+    user=parsed_json['user'],
+    password=parsed_json['password']
+)
+cursor = conn.cursor()
+
+# Get some data.
+sql = "SELECT `id`, `user`, `passwd` FROM users;"
+cursor.execute(sql)
+result = cursor.fetchall()
+print (result)
+
+conn.close()
