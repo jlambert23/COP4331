1044df8123818658dd299b4c866f321e99c29967
coleSil <cole3789@gmail.com>
2018-01-30 16:40:50 -0500

    Added a third user and gave them contacts

--- CreateContactDB.sql
+++ CreateContactDB.sql
@@ -18,29 +18,31 @@ USE `contactDB`;
 CREATE USER IF NOT EXISTS 'db-sys'@'localhost';
 GRANT ALL PRIVILEGES ON `contactDB`.* TO `db-sys`@`localhost`;
 
-CREATE TABLE user (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-userName VARCHAR(30) UNIQUE,
+CREATE TABLE user (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(30) UNIQUE,
 password VARCHAR(64));
 
 CREATE TABLE contact ( firstname VARCHAR(30),
 lastname VARCHAR(30),
 phone VARCHAR(30),
 email VARCHAR(30),
-userID INT);
+userid INT);
 
-# Modify userID to have foreign key.
+# Modify userid to have foreign key.
 ALTER TABLE contact
 ADD CONSTRAINT FK_contact
-FOREIGN KEY (userID) REFERENCES user(id)
+FOREIGN KEY (userid) REFERENCES user(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE;
 
 # Insert base users and data
-INSERT INTO user (userName,password) VALUES ('user','password');
-INSERT INTO user (userName, password) VALUES ('gabe', '9ed1515819dec61fd361d5fdabb57f41ecce1a5fe1fe263b98c0d6943b9b232e');
+INSERT INTO user (username,password) VALUES ('user','password');
+INSERT INTO user (username, password) VALUES ('gabe', '9ed1515819dec61fd361d5fdabb57f41ecce1a5fe1fe263b98c0d6943b9b232e');
+INSERT INTO user (username, password) VALUES ('cole','7842275f98964d3914268cc7b66e35b8b614663978b64a1e9ecb3c5499427ed9');
 
  INSERT INTO contact VALUES ('Cole','Sil','407555555','cole@gmail.com',2);
  INSERT INTO contact VALUES ('James', 'Joyce', '5553456789','jimmy@dublin.com',2);
  INSERT INTO contact VALUES ('Scott', 'Fitzgerald', '5553456789', 'ScottyBoy@gmail.com', 2);
- INSERT INTO contact VALUES ('The', 'Rock', '5553425678', 'peoplesElbow@gmail.com', 2);
+ INSERT INTO contact VALUES ('The', 'Rock', '5553425678', 'peoplesElbow@gmail.com', 3);
  INSERT INTO contact VALUES ('Cole', 'Silvernail', '5555555555', 'cole@gmail.com', 1);
+ INSERT INTO contact VALUES ('Lebron', 'James', '3215555555','cole@gmail.com',3);
